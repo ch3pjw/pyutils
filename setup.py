fdcaccb9d8dfe3ba352f9fbb4857d40630a13550
Paul <paul@ruthorn.co.uk>
2015-12-28 13:06:23 +0000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='pyutils',
+    description=(
+        'Some basic utility functions abstracted out for re-use across '
+        'projects.'),
+    author='Paul Weaver',
+    author_email='paul@ruthorn.co.uk',
+    version='0.01',
+    packages=['pyutils'],
+    install_requires=[],
+    extras_require={
+        'dev': [
+            'nose',
+            'coverage',
+        ]
+    },
+)
